51ac0a6934d30369456d6377a86771169ab1124e
Ian Bogste <ian@bogste.ro>
2016-10-08 17:36:05 +0300

    Minor refactoring for sample1/App.java. Updated README with Video and Presentation links.

--- src/main/java/com/lightbend/akkasample/sample1/App.java
+++ src/main/java/com/lightbend/akkasample/sample1/App.java
@@ -42,12 +42,9 @@ public class App {
     final ActorRef counter = system.actorOf(Counter.props(), "counter");
 
     for (int i = 0; i < 5; i++) {
-      new Thread(new Runnable() {
-        @Override
-        public void run() {
-          for (int j = 0; j < 5; j++) {
-            counter.tell(new Counter.Message(), ActorRef.noSender());
-          }
+      new Thread(() -> {
+        for (int j = 0; j < 5; j++) {
+          counter.tell(new Counter.Message(), ActorRef.noSender());
         }
       }).start();
     }
